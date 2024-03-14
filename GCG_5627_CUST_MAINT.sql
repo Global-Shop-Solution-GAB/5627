a03e7b44073415f58de77c830da41047f048a7d3
Service Web <sw@gssmail.com>
2024-03-14 13:52:47 -0500

    Commit changes in C:\Websites\Git\emarlow\5627\GCG_5627_CUST_MAINT.sql

---
+++ GCG_5627_CUST_MAINT.sql
@@ -0,0 +1,14 @@
+/* ==========================================
+ * TABLE: GCG_5627_CUST_MAINT
+ * ========================================== */
+CREATE TABLE "GCG_5627_CUST_MAINT"(
+ "CUSTOMER" CHAR(6),
+ "CUSTOMER_NAME" CHAR(30),
+ "SHIP_TO" CHAR(6),
+ "CITY" CHAR(20),
+ "STATE" CHAR(20),
+ "PRODUCT_LINE" CHAR(2),
+ "PL_DISCNT" NUMERIC(3,2),
+ "CUST_MARGIN" NUMERIC(3,2));
+
+
